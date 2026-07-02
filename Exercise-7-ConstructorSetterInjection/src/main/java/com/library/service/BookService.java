38e17bd143bdd621623dff7209586d56a8e4bda0
sudeepa77 <sudeepareddy295@gmail.com>
2026-07-02 14:32:46 +0530

    Completed Exercise 7 - Constructor and Setter Injection

---
+++ Exercise-7-ConstructorSetterInjection/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,27 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Constructor Injection
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayService() {
+        System.out.println("BookService bean is created.");
+        bookRepository.displayRepository();
+    }
+}
